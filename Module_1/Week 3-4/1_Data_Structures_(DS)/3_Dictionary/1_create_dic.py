ed24821f7227d54f3a69e7eda6c2f0ce207f7b19
atif <matif2k13@gmail.com>
2025-02-13 20:28:58 +0500

    3_dictionary folder has added

---
+++ Module_1/Week 3-4/1_Data_Structures_(DS)/3_Dictionary/1_create_dic.py
@@ -0,0 +1,15 @@
+# Empty dictionary
+my_dict = {}
+print(my_dict,"my_dict")
+
+# Dictionary with integer keys
+int_keys = {1: "One", 2: "Two", 3: "Three"}
+print(int_keys, "int_keys")
+
+# Dictionary with mixed keys
+mixed_keys = {"name": "Alice", 1: [2, 3, 4]}
+print(mixed_keys,"mixed_keys")
+
+# Nested dictionary
+nested_dict = {"course": "Python", "details": {"duration": "3 months", "level": "Beginner"}}
+print(nested_dict,"nested_dict")
