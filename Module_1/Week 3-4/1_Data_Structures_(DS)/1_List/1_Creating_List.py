a0eb57231667117994ca844fa12aa3263a9c1217
atif <matif2k13@gmail.com>
2025-02-13 19:54:45 +0500

    1_Creating_List.py is added

---
+++ Module_1/Week 3-4/1_Data_Structures_(DS)/1_List/1_Creating_List.py
@@ -0,0 +1,14 @@
+# Empty list
+empty_list = []
+print("List having no items\n", empty_list)
+# List with integers
+int_list = [1, 2, 3, 4, 5]
+print("List with homogenous items\n",int_list)
+
+# List with mixed data types
+mixed_list = [1, "hello", 3.5, True]
+print("List with haterogenous items\n", mixed_list)
+
+# Nested list (list inside a list)
+nested_list = [[1, 2], [3, 4], [5, 6]]
+print("List inside list\n",nested_list)
