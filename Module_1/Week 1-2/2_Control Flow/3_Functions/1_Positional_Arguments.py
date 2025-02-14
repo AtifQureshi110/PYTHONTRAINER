a5d4163c868d378eb402bdbce25cd58226d430cb
atif <matif2k13@gmail.com>
2025-02-14 14:52:55 +0500

    Positional_Arguments_has_added

---
+++ Module_1/Week 1-2/2_Control Flow/3_Functions/1_Positional_Arguments.py
@@ -0,0 +1,6 @@
+# These are passed in the same order as defined in the function.
+# The number and order of arguments must match the function definition.
+
+def greet(name, age):
+    print(f"Hello {name}, you are {age} years old.")
+greet("John", 25)
