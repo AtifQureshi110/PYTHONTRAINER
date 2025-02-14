8cc8262b1e274d27b7af3181bfcb44a8f9160e53
atif <matif2k13@gmail.com>
2025-02-14 14:15:45 +0500

    syntax_and_practical_example_has_added

---
+++ Module_1/Week 1-2/2_Control Flow/2_Loops_(For and While)/1_For_Loop.py
@@ -0,0 +1,13 @@
+# definition
+# The for loop is used for iterating over a sequence (list, tuple, string, or range). 
+# It executes a block of code for each element in the sequence.
+
+
+#syntax
+# for variable in sequence:
+#     # Block of code
+
+
+#practical example
+for num in range(1, 6):
+    print(num)
