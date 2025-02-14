a27d7d5bf05215b08855b3f61147a3952f2ada49
atif <matif2k13@gmail.com>
2025-02-14 14:19:00 +0500

    while_loop_practical_

---
+++ Module_1/Week 1-2/2_Control Flow/2_Loops_(For and While)/2_While_Loop.py
@@ -0,0 +1,14 @@
+# Definition 
+# The while loop repeats a block of code as long as a specified condition is true.
+
+
+#syntax
+# while condition:
+#     # Block of code
+
+
+#practical example 
+count = 1
+while count <= 5:
+    print(count)
+    count += 1
