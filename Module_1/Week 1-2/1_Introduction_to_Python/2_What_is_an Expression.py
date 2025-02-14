c9005a6fc074e75200c613a7539e3e4866bcf5cd
atif <matif2k13@gmail.com>
2025-02-14 11:39:34 +0500

    expression code is added

---
+++ Module_1/Week 1-2/1_Introduction_to_Python/2_What_is_an Expression.py
@@ -0,0 +1,8 @@
+# An expression is a combination of variables, operators, and values that produces a result.
+x = 10 + 5      
+y = x * 2       
+z = (x + y) / 3 
+print("Expression_1 with addition: ", x)
+print("Expression_2 with multiplication: ", y)
+print("Expression_3 with division: ", z)
+# Expressions can be simple or complex and are used to compute values.
