6d010de69f35c4c8c42736d8b9aac96200266e31
atif <matif2k13@gmail.com>
2025-02-14 13:47:29 +0500

    syntax of elif has added

---
+++ Module_1/Week 1-2/2_Control Flow/1_If_else/3_Syntax_elif_Statement_(Multiple_Conditions).py
@@ -0,0 +1,6 @@
+# if condition1:
+#     # Block of code if condition1 is true
+# elif condition2:
+#     # Block of code if condition2 is true
+# else:
+#     # Block of code if none of the conditions are true
