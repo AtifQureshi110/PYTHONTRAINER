bb108a461627b80c3fdd7f84fda58b86a9d91c05
atif <matif2k13@gmail.com>
2025-02-14 14:21:55 +0500

    loop_with_control_state_has_added

---
+++ Module_1/Week 1-2/2_Control Flow/2_Loops_(For and While)/3_Loop_Control_Statements.py
@@ -0,0 +1,10 @@
+# Python provides control statements to alter the loop execution. These include:
+
+# break – Exits the loop prematurely
+# continue – Skips the current iteration and continues with the next iteration
+# pass – Does nothing, acts as a placeholder
+
+for i in range(1, 6):
+    if i == 4:
+        break
+    print(i)
