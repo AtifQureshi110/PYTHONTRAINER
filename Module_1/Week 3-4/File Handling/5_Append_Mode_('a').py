5889da5af1784dec12526c798a4c4156111ad92a
atif <matif2k13@gmail.com>
2025-02-14 10:11:45 +0500

    some more content and data is added to the to the text file append(a) mode

---
+++ Module_1/Week 3-4/File Handling/5_Append_Mode_('a').py
@@ -0,0 +1,3 @@
+# Appends data to the file without overwriting the existing content.
+file = open(r"E:\PYTHONTRAINER\Module_1\Week 3-4\File Handling\2_example.txt", "a")
+file.write("\nAppended content.")
