b7b50393fbf13b13fefa8ecba705b958def117e4
atif <matif2k13@gmail.com>
2025-02-17 14:51:00 +0500

    mathematical_operations_with_data_added

---
+++ Module_2/Week 5-7/numpy/mathematical_operations_with_data.py
@@ -0,0 +1,8 @@
+import numpy as np
+data = np.random.randn(2, 3)
+print(f"Random data: {data}\n")
+
+print(f"Addition: {data+data}\n")
+print(f"Subraction: {data-data}\n")
+print(f"Multiplication: {data*10}\n")
+print(f"Division: {data/10}\n")
