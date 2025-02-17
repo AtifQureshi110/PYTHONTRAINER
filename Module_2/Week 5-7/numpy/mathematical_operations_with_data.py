91db1cffda894c4b8e2f6c946c97abd2b3a96c8a
atif <matif2k13@gmail.com>
2025-02-17 15:03:12 +0500

    multiple_demension_array_has_added

--- Module_2/Week 5-7/numpy/mathematical_operations_with_data.py
+++ Module_2/Week 5-7/numpy/mathematical_operations_with_data.py
@@ -6,3 +6,6 @@ print(f"Addition: {data+data}\n")
 print(f"Subraction: {data-data}\n")
 print(f"Multiplication: {data*10}\n")
 print(f"Division: {data/10}\n")
+
+print(f"The shape of arrary is: {data.shape}\n")
+print(f"The data type of arrary is: {data.dtype}")
